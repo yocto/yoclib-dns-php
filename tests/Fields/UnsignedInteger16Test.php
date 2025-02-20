5c0b58f74c7bb35b1957bbdca9a827edb51e4480
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-02-20 12:13:30 +0100

    Add all tests for UInt16

---
+++ tests/Fields/UnsignedInteger16Test.php
@@ -0,0 +1,121 @@
+<?php
+namespace YOCLIB\DNS\Tests\Fields;
+
+use PHPUnit\Framework\TestCase;
+
+use YOCLIB\DNS\Exceptions\DNSFieldException;
+use YOCLIB\DNS\Fields\UnsignedInteger16;
+
+class UnsignedInteger16Test extends TestCase{
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testConstructor(): void{
+        self::assertInstanceOf(UnsignedInteger16::class,new UnsignedInteger16(0));
+        self::assertInstanceOf(UnsignedInteger16::class,new UnsignedInteger16(32639));
+        self::assertInstanceOf(UnsignedInteger16::class,new UnsignedInteger16(32896));
+        self::assertInstanceOf(UnsignedInteger16::class,new UnsignedInteger16(65535));
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testTooLow(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Human readable UInt16 should be in the range of 0 and 65535.');
+
+        new UnsignedInteger16(-131072);
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testTooHigh(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Human readable UInt16 should be in the range of 0 and 65535.');
+
+        new UnsignedInteger16(131072);
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testGetValue(): void{
+        self::assertSame(0,(new UnsignedInteger16(0))->getValue());
+        self::assertSame(32639,(new UnsignedInteger16(32639))->getValue());
+        self::assertSame(32896,(new UnsignedInteger16(32896))->getValue());
+        self::assertSame(65535,(new UnsignedInteger16(65535))->getValue());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testSerializeToPresentationFormat(): void{
+        self::assertSame('0',(new UnsignedInteger16(0))->serializeToPresentationFormat());
+        self::assertSame('32639',(new UnsignedInteger16(32639))->serializeToPresentationFormat());
+        self::assertSame('32896',(new UnsignedInteger16(32896))->serializeToPresentationFormat());
+        self::assertSame('65535',(new UnsignedInteger16(65535))->serializeToPresentationFormat());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testSerializeToWireFormat(): void{
+        self::assertSame("\x00\x00",(new UnsignedInteger16(0))->serializeToWireFormat());
+        self::assertSame("\x7F\x7F",(new UnsignedInteger16(32639))->serializeToWireFormat());
+        self::assertSame("\x80\x80",(new UnsignedInteger16(32896))->serializeToWireFormat());
+        self::assertSame("\xFF\xFF",(new UnsignedInteger16(65535))->serializeToWireFormat());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromPresentationFormat(): void{
+        self::assertSame(0,UnsignedInteger16::deserializeFromPresentationFormat('0')->getValue());
+        self::assertSame(32639,UnsignedInteger16::deserializeFromPresentationFormat('32639')->getValue());
+        self::assertSame(32896,UnsignedInteger16::deserializeFromPresentationFormat('32896')->getValue());
+        self::assertSame(65535,UnsignedInteger16::deserializeFromPresentationFormat('65535')->getValue());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromWireFormat(): void{
+        self::assertSame(0,UnsignedInteger16::deserializeFromWireFormat("\x00\x00")->getValue());
+        self::assertSame(32639,UnsignedInteger16::deserializeFromWireFormat("\x7F\x7F")->getValue());
+        self::assertSame(32896,UnsignedInteger16::deserializeFromWireFormat("\x80\x80")->getValue());
+        self::assertSame(65535,UnsignedInteger16::deserializeFromWireFormat("\xFF\xFF")->getValue());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromWireFormatTooLessData(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Binary UInt16 should be 2 octets.');
+
+        UnsignedInteger16::deserializeFromWireFormat("\xAA");
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromWireFormatTooMuchData(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Binary UInt16 should be 2 octets.');
+
+        UnsignedInteger16::deserializeFromWireFormat("\xAA\xBB\xCC\xDD");
+    }
+
+}
--- tests/FieldsTest.php
+++ tests/FieldsTest.php
@@ -14,7 +14,6 @@ class FieldsTest extends TestCase{
      * @throws DNSFieldException
      */
     public function testGetValue(): void{
-        self::assertSame(1234,(new UnsignedInteger16(1234))->getValue());
         self::assertSame(12345678,(new UnsignedInteger32(12345678))->getValue());
     }
 
@@ -23,7 +22,6 @@ class FieldsTest extends TestCase{
      * @throws DNSFieldException
      */
     public function testSerializeToPresentationFormat(): void{
-        self::assertSame('1234',(new UnsignedInteger16(1234))->serializeToPresentationFormat());
         self::assertSame('12345678',(new UnsignedInteger32(12345678))->serializeToPresentationFormat());
     }
 
@@ -32,7 +30,6 @@ class FieldsTest extends TestCase{
      * @throws DNSFieldException
      */
     public function testSerializeToWireFormat(): void{
-        self::assertSame(pack('n',1234),(new UnsignedInteger16(1234))->serializeToWireFormat());
         self::assertSame(pack('N',12345678),(new UnsignedInteger32(12345678))->serializeToWireFormat());
     }
 
@@ -41,9 +38,6 @@ class FieldsTest extends TestCase{
      * @throws DNSFieldException
      */
     public function testAll(): void{
-        self::assertEquals("\x7F\x7F",UnsignedInteger16::deserializeFromPresentationFormat('32639')->serializeToWireFormat());
-        self::assertEquals("32639",UnsignedInteger16::deserializeFromWireFormat("\x7F\x7F")->serializeToPresentationFormat());
-
         self::assertEquals("\x7F\x7F\x7F\x7F",UnsignedInteger32::deserializeFromPresentationFormat('2139062143')->serializeToWireFormat());
         self::assertEquals("2139062143",UnsignedInteger32::deserializeFromWireFormat("\x7F\x7F\x7F\x7F")->serializeToPresentationFormat());
     }
