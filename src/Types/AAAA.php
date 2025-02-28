77bd8274407c1e69fbfcdcf7cb4329dddef860c8
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-02-28 15:35:10 +0100

    Implement deserialization for AAAA records

--- src/Types/AAAA.php
+++ src/Types/AAAA.php
@@ -1,9 +1,11 @@
 <?php
 namespace YOCLIB\DNS\Types;
 
+use YOCLIB\DNS\Exceptions\DNSFieldException;
 use YOCLIB\DNS\Exceptions\DNSTypeException;
 use YOCLIB\DNS\Fields\Field;
 use YOCLIB\DNS\Fields\IPv6Address;
+use YOCLIB\DNS\LineLexer;
 
 class AAAA extends Type{
 
@@ -21,12 +23,38 @@ class AAAA extends Type{
         }
     }
 
+    /**
+     * @param string $data
+     * @return AAAA
+     * @throws DNSFieldException
+     * @throws DNSTypeException
+     */
     public static function deserializeFromPresentationFormat(string $data): AAAA{
-        throw new \RuntimeException('Not implemented');
+        $tokens = LineLexer::tokenizeLine($data);
+        if(count($tokens)!==1){
+            throw new DNSTypeException('A record should contain 1 field.');
+        }
+        return new self([
+            IPv6Address::deserializeFromPresentationFormat($tokens[0]),
+        ]);
     }
 
+    /**
+     * @param string $data
+     * @return AAAA
+     * @throws DNSFieldException
+     * @throws DNSTypeException
+     */
     public static function deserializeFromWireFormat(string $data): AAAA{
-        throw new \RuntimeException('Not implemented');
+        $offset = 0;
+        $address = substr($data,$offset,IPv6Address::calculateLength($data));
+        $remaining = substr($data,$offset);
+        if(strlen($remaining)>0){
+            throw new DNSTypeException('Cannot have remaining data.');
+        }
+        return new self([
+            IPv6Address::deserializeFromWireFormat($address),
+        ]);
     }
 
 }
