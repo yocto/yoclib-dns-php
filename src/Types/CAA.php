25900c152e7923301b2a67769ca638093c76fa31
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-03-01 00:25:55 +0100

    Implement CAA deserialization

--- src/Types/CAA.php
+++ src/Types/CAA.php
@@ -1,11 +1,13 @@
 <?php
 namespace YOCLIB\DNS\Types;
 
+use YOCLIB\DNS\Exceptions\DNSFieldException;
 use YOCLIB\DNS\Exceptions\DNSTypeException;
 use YOCLIB\DNS\Fields\Binary;
 use YOCLIB\DNS\Fields\CharacterString;
 use YOCLIB\DNS\Fields\Field;
 use YOCLIB\DNS\Fields\UnsignedInteger8;
+use YOCLIB\DNS\LineLexer;
 
 class CAA extends Type{
 
@@ -29,12 +31,58 @@ class CAA extends Type{
         }
     }
 
-    public static function deserializeFromPresentationFormat(string $data): CAA{
-        throw new \RuntimeException('Not implemented');
+    /**
+     * @return string
+     * @throws DNSFieldException
+     */
+    public function serializeToPresentationFormat(): string{
+        return implode(' ',[
+            $this->getFields()[0]->serializeToPresentationFormat(),
+            $this->getFields()[1]->serializeToPresentationFormat(),
+            (new CharacterString($this->getFields()[2]->getValue()))->serializeToPresentationFormat(),
+        ]);
     }
 
+    /**
+     * @param string $data
+     * @return CAA
+     * @throws DNSFieldException
+     * @throws DNSTypeException
+     */
+    public static function deserializeFromPresentationFormat(string $data): CAA{
+        $tokens = LineLexer::tokenizeLine($data);
+        if(count($tokens)!==3){
+            throw new DNSTypeException('CAA record should contain 3 fields.');
+        }
+        return new self([
+            UnsignedInteger8::deserializeFromPresentationFormat($tokens[0]),
+            CharacterString::deserializeFromPresentationFormat($tokens[1]),
+            new Binary(CharacterString::deserializeFromPresentationFormat($tokens[2])->getValue()),
+        ]);
+    }
+
+    /**
+     * @param string $data
+     * @return CAA
+     * @throws DNSFieldException
+     * @throws DNSTypeException
+     */
     public static function deserializeFromWireFormat(string $data): CAA{
-        throw new \RuntimeException('Not implemented');
+        $offset = 0;
+
+        $flags = substr($data,$offset,UnsignedInteger8::calculateLength(substr($data,$offset)));
+        $offset += strlen($flags);
+
+        $tag = substr($data,$offset,CharacterString::calculateLength(substr($data,$offset)));
+        $offset += strlen($tag);
+
+        $value = substr($data,$offset);
+
+        return new self([
+            UnsignedInteger8::deserializeFromWireFormat($flags),
+            CharacterString::deserializeFromWireFormat($tag),
+            Binary::deserializeFromWireFormat($value),
+        ]);
     }
 
 }
