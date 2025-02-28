3858a910609ec80124d0188401a397d10fa4505e
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-02-28 12:31:46 +0100

    Add SPF type

---
+++ src/Types/SPF.php
@@ -0,0 +1,31 @@
+<?php
+namespace YOCLIB\DNS\Types;
+
+use YOCLIB\DNS\Exceptions\DNSTypeException;
+use YOCLIB\DNS\Fields\CharacterString;
+use YOCLIB\DNS\Fields\Field;
+
+class SPF extends Type{
+
+    /**
+     * @param array|Field[] $fields
+     * @throws DNSTypeException
+     */
+    public function __construct(array $fields){
+        parent::__construct($fields);
+        if(count($fields)<1){
+            throw new DNSTypeException('At least one field required.');
+        }
+        $totalLength = 0;
+        foreach($fields as $field){
+            if(!($field instanceof CharacterString)){
+                throw new DNSTypeException('Every field should be a character string.');
+            }
+            $totalLength += strlen($field->serializeToWireFormat());
+        }
+        if($totalLength>65536){
+            throw new DNSTypeException('Maximum size exceeded.');
+        }
+    }
+
+}
