9f16e9e5d43c0bef25a820aa0946a96e08661ce8
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-02-28 15:33:09 +0100

    Implement deserialization for A records

--- src/Types/A.php
+++ src/Types/A.php
@@ -2,9 +2,11 @@
 namespace YOCLIB\DNS\Types;
 
 use YOCLIB\DNS\DNSClass;
+use YOCLIB\DNS\Exceptions\DNSFieldException;
 use YOCLIB\DNS\Exceptions\DNSTypeException;
 use YOCLIB\DNS\Fields\Field;
 use YOCLIB\DNS\Fields\IPv4Address;
+use YOCLIB\DNS\LineLexer;
 
 class A extends Type{
 
@@ -25,12 +27,38 @@ class A extends Type{
         }
     }
 
+    /**
+     * @param string $data
+     * @return A
+     * @throws DNSFieldException
+     * @throws DNSTypeException
+     */
     public static function deserializeFromPresentationFormat(string $data): A{
-        throw new \RuntimeException('Not implemented');
+        $tokens = LineLexer::tokenizeLine($data);
+        if(count($tokens)!==1){
+            throw new DNSTypeException('A record should contain 1 field.');
+        }
+        return new self([
+            IPv4Address::deserializeFromPresentationFormat($tokens[0]),
+        ]);
     }
 
+    /**
+     * @param string $data
+     * @return A
+     * @throws DNSFieldException
+     * @throws DNSTypeException
+     */
     public static function deserializeFromWireFormat(string $data): A{
-        throw new \RuntimeException('Not implemented');
+        $offset = 0;
+        $address = substr($data,$offset,IPv4Address::calculateLength($data));
+        $remaining = substr($data,$offset);
+        if(strlen($remaining)>0){
+            throw new DNSTypeException('Cannot have remaining data.');
+        }
+        return new self([
+            IPv4Address::deserializeFromWireFormat($address),
+        ]);
     }
 
 }
