5b4ba6f7f4ad97ab0755b5ca6d279c42b97aba3a
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-03-10 11:04:54 +0100

    Add window block bitmap

---
+++ src/Fields/WindowBlockBitmap.php
@@ -0,0 +1,138 @@
+<?php
+namespace YOCLIB\DNS\Fields;
+
+use YOCLIB\DNS\Exceptions\DNSFieldException;
+
+class WindowBlockBitmap implements Field{
+
+    private array $value;
+
+    /**
+     * @param array|int[] $value
+     * @throws DNSFieldException
+     */
+    public function __construct(array $value){
+        $foundBits = [];
+        foreach($value AS $bit){
+            if(in_array($bit,$foundBits)){
+                throw new DNSFieldException('No duplicate bits allowed.');
+            }
+            $foundBits[] = $bit;
+            if(!is_int($bit)){
+                throw new DNSFieldException('Only integers allowed.');
+            }
+            if($bit<0){
+                throw new DNSFieldException('Only positive integers allowed.');
+            }
+        }
+        $this->value = $value;
+        sort($this->value);
+    }
+
+    /**
+     * @return array|int[]
+     */
+    public function getValue(): array{
+        return $this->value;
+    }
+
+    /**
+     * @param ?array|string[]|null $mapping
+     * @return string
+     */
+    public function serializeToPresentationFormat(?array $mapping=null): string{
+        $items = [];
+        foreach($this->value AS $bit){
+            $items[] = $mapping[$bit] ?? strval($bit);
+        }
+        return implode(' ',$items);
+    }
+
+    /**
+     * @return string
+     */
+    public function serializeToWireFormat(): string{
+        $windowBlockBytes = [];
+        foreach($this->value AS $bit){
+            $windowBlock = intval($bit/256);
+            $byte = intval(($bit-(256*$windowBlock))/8);
+            if(!array_key_exists($byte,$windowBlockBytes[$windowBlock] ?? [])){
+                $windowBlockBytes[$windowBlock][$byte] = 0x00;
+            }
+            $windowBlockBytes[$windowBlock][$byte] |= 1<<($bit%8);
+        }
+        $bitmap = '';
+        foreach($windowBlockBytes AS $windowBlock=>$bytes){
+            $windowBlockBitmap = '';
+            if(count($bytes)){
+                for($i=0;$i<max(array_keys($bytes))+1;$i++){
+                    $windowBlockBitmap[$i] = chr(0x00 | ($bytes[$i] ?? 0x00));
+                }
+                $bitmap .= chr($windowBlock).chr(strlen($windowBlockBitmap)).$windowBlockBitmap;
+            }
+        }
+        return $bitmap;
+    }
+
+    /**
+     * @param string $data
+     * @return int
+     */
+    public static function calculateLength(string $data): int{
+        return strlen($data);
+    }
+
+    /**
+     * @param string $data
+     * @param ?array|string[]|null $mapping
+     * @return WindowBlockBitmap
+     * @throws DNSFieldException
+     */
+    public static function deserializeFromPresentationFormat(string $data,?array $mapping=null): WindowBlockBitmap{
+        $bits = [];
+        foreach(explode(' ',$data) AS $item){
+            foreach($mapping AS $bit=>$map){
+                if($map===$item){
+                    $bits[] = $bit;
+                    break;
+                }
+            }
+        }
+        return new self($bits);
+    }
+
+    /**
+     * @param string $data
+     * @return WindowBlockBitmap
+     * @throws DNSFieldException
+     */
+    public static function deserializeFromWireFormat(string $data): WindowBlockBitmap{
+        $bits = [];
+        for($i=0;$i<strlen($data);){
+            $windowBlock = ord($data[$i]);
+            $i++;
+            if($i>=strlen($data)){
+                throw new DNSFieldException('Too less data to read window block length.');
+            }
+            $bitmapLength = ord($data[$i]);
+            $i++;
+            $bitmapData = substr($data,$i,$bitmapLength);
+            $i += strlen($bitmapData);
+            if(strlen($bitmapData)!==$bitmapLength){
+                throw new DNSFieldException('Too less data to read window block bytes.');
+            }
+            $bytes = str_split($bitmapData);
+            for($j=0;$j<count($bytes);$j++){
+                $octet = ord($bytes[$j]);
+                for($k=0;$k<8;$k++){
+                    $bit = ($octet>>$k) & 0b1;
+                    if($bit){
+                        $bits[] = $windowBlock*256 + $j*8 + $k;
+                    }
+                }
+            }
+        }
+        return new self($bits);
+    }
+
+}
---
+++ tests/Fields/WindowBlockBitmapTest.php
@@ -0,0 +1,134 @@
+<?php
+namespace YOCLIB\DNS\Tests\Fields;
+
+use PHPUnit\Framework\TestCase;
+
+use YOCLIB\DNS\Exceptions\DNSFieldException;
+use YOCLIB\DNS\Fields\WindowBlockBitmap;
+
+class WindowBlockBitmapTest extends TestCase{
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testConstructor(): void{
+        self::assertInstanceOf(WindowBlockBitmap::class,new WindowBlockBitmap([1,2,3,4]));
+        self::assertInstanceOf(WindowBlockBitmap::class,new WindowBlockBitmap([1,2,3,4,5]));
+        self::assertInstanceOf(WindowBlockBitmap::class,new WindowBlockBitmap([1,2,3,4,5,6]));
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDuplicateBits(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('No duplicate bits allowed.');
+
+        new WindowBlockBitmap([1,2,3,3,4,5,6]);
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testNonIntegerBits(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Only integers allowed.');
+
+        new WindowBlockBitmap([1,2,'3',4,5,6]);
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testNegativeIntegers(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Only positive integers allowed.');
+
+        new WindowBlockBitmap([1,2,-3,4,5,6]);
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testGetValue(): void{
+        self::assertSame([1,2,3,4],(new WindowBlockBitmap([1,2,3,4]))->getValue());
+        self::assertSame([1,2,3,4,5],(new WindowBlockBitmap([1,2,3,4,5]))->getValue());
+        self::assertSame([1,2,3,4,5,6],(new WindowBlockBitmap([1,2,3,4,5,6]))->getValue());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testSerializeToPresentationFormat(): void{
+        $mapping = [
+            1 => 'ONE',
+            2 => 'TWO',
+            3 => 'THREE',
+            4 => 'FOUR',
+            5 => 'FIVE',
+            6 => 'SIX',
+        ];
+
+        self::assertSame('ONE TWO THREE FOUR',(new WindowBlockBitmap([1,2,3,4]))->serializeToPresentationFormat($mapping));
+        self::assertSame('ONE TWO THREE FOUR FIVE',(new WindowBlockBitmap([1,2,3,4,5]))->serializeToPresentationFormat($mapping));
+        self::assertSame('ONE TWO THREE FOUR FIVE SIX',(new WindowBlockBitmap([1,2,3,4,5,6]))->serializeToPresentationFormat($mapping));
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testSerializeToWireFormat(): void{
+        self::assertSame("\x00\x01\x1E",(new WindowBlockBitmap([1,2,3,4]))->serializeToWireFormat());
+        self::assertSame("\x00\x01\x3E",(new WindowBlockBitmap([1,2,3,4,5]))->serializeToWireFormat());
+        self::assertSame("\x00\x01\x7E",(new WindowBlockBitmap([1,2,3,4,5,6]))->serializeToWireFormat());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCalculateLength(): void{
+        self::assertSame(1,WindowBlockBitmap::calculateLength("\x1E"));
+        self::assertSame(14,WindowBlockBitmap::calculateLength("\x1EtrailingBytes"));
+        self::assertSame(1,WindowBlockBitmap::calculateLength("\x3E"));
+        self::assertSame(14,WindowBlockBitmap::calculateLength("\x3EtrailingBytes"));
+        self::assertSame(1,WindowBlockBitmap::calculateLength("\x7E"));
+        self::assertSame(14,WindowBlockBitmap::calculateLength("\x7EtrailingBytes"));
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromPresentationFormat(): void{
+        $mapping = [
+            1 => 'ONE',
+            2 => 'TWO',
+            3 => 'THREE',
+            4 => 'FOUR',
+            5 => 'FIVE',
+            6 => 'SIX',
+        ];
+
+        self::assertSame([1,2,3,4],WindowBlockBitmap::deserializeFromPresentationFormat('ONE TWO THREE FOUR',$mapping)->getValue());
+        self::assertSame([1,2,3,4,5],WindowBlockBitmap::deserializeFromPresentationFormat('ONE TWO THREE FOUR FIVE',$mapping)->getValue());
+        self::assertSame([1,2,3,4,5,6],WindowBlockBitmap::deserializeFromPresentationFormat('ONE TWO THREE FOUR FIVE SIX',$mapping)->getValue());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromWireFormat(): void{
+        self::assertSame([1,2,3,4],WindowBlockBitmap::deserializeFromWireFormat("\x00\x01\x1E")->getValue());
+        self::assertSame([1,2,3,4,5],WindowBlockBitmap::deserializeFromWireFormat("\x00\x01\x3E")->getValue());
+        self::assertSame([1,2,3,4,5,6],WindowBlockBitmap::deserializeFromWireFormat("\x00\x01\x7E")->getValue());
+    }
+
+}
