0e665aa028964081e55b0322aed9c989661d5cb5
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-03-12 12:21:17 +0100

    Add address prefix field

---
+++ src/Fields/AddressPrefix.php
@@ -0,0 +1,120 @@
+<?php
+namespace YOCLIB\DNS\Fields;
+
+use YOCLIB\DNS\Exceptions\DNSFieldException;
+
+class AddressPrefix implements Field{
+
+    private array $value;
+
+    /**
+     * @param array|string[] $value
+     * @throws DNSFieldException
+     */
+    public function __construct(string... $value){
+        if(count($value)!==4){
+            throw new DNSFieldException('The value should have 4 elements.');
+        }
+        $addressFamily = intval($value[0] ?? '');
+        if($addressFamily!==1 && $addressFamily!==2){
+            throw new DNSFieldException('Only IPv4 and IPv6 address families are supported.');
+        }
+        $prefix = intval($value[1] ?? '');
+        if($prefix<0){
+            throw new DNSFieldException('Prefix should be positive.');
+        }
+        if($addressFamily===1 && $prefix>32){
+            throw new DNSFieldException('Prefix cannot be larger than 32 for IPv4 addresses.');
+        }
+        if($addressFamily===2 && $prefix>128){
+            throw new DNSFieldException('Prefix cannot be larger than 128 for IPv6 addresses.');
+        }
+        $value[2] = boolval($value[2] ?? '');
+        $address = $value[3] ?? '';
+        if($addressFamily===1 && !filter_var($address,FILTER_VALIDATE_IP,FILTER_FLAG_IPV4)){
+            throw new DNSFieldException('Human readable IPv4 address should have 4 unsigned integers ranging from 0 to 255, all seperated by dot.');
+        }
+        if($addressFamily===2 && !filter_var($address,FILTER_VALIDATE_IP,FILTER_FLAG_IPV6)){
+            throw new DNSFieldException('Human readable IPv6 address isn\'t valid.');
+        }
+        $this->value = $value;
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function getValue(): array{
+        return $this->value;
+    }
+
+    /**
+     * @return string
+     */
+    public function serializeToPresentationFormat(): string{
+        return ($this->value[2]?'!':'').$this->value[0].':'.$this->value[3].'/'.$this->value[1];
+    }
+
+    /**
+     * @return string
+     */
+    public function serializeToWireFormat(): string{
+        $address = rtrim(inet_pton($this->value[3]),"\x00");
+        $length = strlen($address);
+        if($this->value[2]){
+            $length |= 0x80;
+        }
+        return pack('n',intval($this->value[0])).chr(intval($this->value[1])).chr($length).$address;
+    }
+
+    /**
+     * @param string $data
+     * @return int
+     */
+    public static function calculateLength(string $data): int{
+        return 4+(ord($data[3] ?? "\x00")&0x7F);
+    }
+
+    /**
+     * @param string $data
+     * @return AddressPrefix
+     * @throws DNSFieldException
+     */
+    public static function deserializeFromPresentationFormat(string $data): AddressPrefix{
+        if(!preg_match('/(!?)(\d+):([A-Za-z0-9.:]+)\/(\d+)/',$data,$matches)){
+            throw new DNSFieldException('Invalid address prefix format.');
+        }
+        return new self($matches[2],$matches[4],$matches[1],$matches[3]);
+    }
+
+    /**
+     * @param string $data
+     * @return AddressPrefix
+     * @throws DNSFieldException
+     */
+    public static function deserializeFromWireFormat(string $data): AddressPrefix{
+        if(strlen($data)<=4){
+            throw new DNSFieldException('Address Prefix should be at least 4 octets.');
+        }
+        $offset = 0;
+
+        $addressFamily = substr($data,$offset,UnsignedInteger16::calculateLength(substr($data,$offset)));
+        $offset += strlen($addressFamily);
+        $addressFamilyValue = UnsignedInteger16::deserializeFromWireFormat($addressFamily)->getValue();
+
+        $prefix = substr($data,$offset,UnsignedInteger8::calculateLength(substr($data,$offset)));
+        $offset += strlen($prefix);
+        $prefixValue = UnsignedInteger8::deserializeFromWireFormat($prefix)->getValue();
+
+        $addressLength = substr($data,$offset,UnsignedInteger8::calculateLength(substr($data,$offset)));
+        $offset += strlen($addressLength);
+        $addressLengthValue = UnsignedInteger8::deserializeFromWireFormat($addressLength)->getValue();
+
+        $negation = ($addressLengthValue>>7)&0b1;
+
+        $address = substr($data,$offset,$addressLengthValue & 0x7F);
+        $address = str_pad($address,$addressFamilyValue==1?4:16,"\x00");
+
+        return new self($addressFamilyValue,$prefixValue,$negation,inet_ntop($address));
+    }
+
+}
---
+++ tests/Fields/AddressPrefixTest.php
@@ -0,0 +1,79 @@
+<?php
+namespace YOCLIB\DNS\Tests\Fields;
+
+use PHPUnit\Framework\TestCase;
+
+use YOCLIB\DNS\Exceptions\DNSFieldException;
+use YOCLIB\DNS\Fields\AddressPrefix;
+
+class AddressPrefixTest extends TestCase{
+
+    /**
+     * @return void
+     */
+    public function testConstructor(): void{
+        self::assertInstanceOf(AddressPrefix::class,new AddressPrefix('1','21',false,'192.168.32.0'));
+        self::assertInstanceOf(AddressPrefix::class,new AddressPrefix('1','28',true,'192.168.38.0'));
+        self::assertInstanceOf(AddressPrefix::class,new AddressPrefix('2','8',false,'FF00:0:0:0:0:0:0:0'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetValue(): void{
+        self::assertSame(['1','21',false,'192.168.32.0'],(new AddressPrefix('1','21',false,'192.168.32.0'))->getValue());
+        self::assertSame(['1','28',true,'192.168.38.0'],(new AddressPrefix('1','28',true,'192.168.38.0'))->getValue());
+        self::assertSame(['2','8',false,'FF00:0:0:0:0:0:0:0'],(new AddressPrefix('2','8',false,'FF00:0:0:0:0:0:0:0'))->getValue());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSerializeToPresentationFormat(): void{
+        self::assertSame('1:192.168.32.0/21',(new AddressPrefix('1','21',false,'192.168.32.0'))->serializeToPresentationFormat());
+        self::assertSame('!1:192.168.38.0/28',(new AddressPrefix('1','28',true,'192.168.38.0'))->serializeToPresentationFormat());
+        self::assertSame('2:FF00:0:0:0:0:0:0:0/8',(new AddressPrefix('2','8',false,'FF00:0:0:0:0:0:0:0'))->serializeToPresentationFormat());
+    }
+
+    /**
+     * @return void
+     */
+    public function testSerializeToWireFormat(): void{
+        self::assertSame("\x00\x01\x15\x03\xC0\xA8\x20",(new AddressPrefix('1','21',false,'192.168.32.0'))->serializeToWireFormat());
+        self::assertSame("\x00\x01\x1C\x83\xC0\xA8\x26",(new AddressPrefix('1','28',true,'192.168.38.0'))->serializeToWireFormat());
+        self::assertSame("\x00\x02\x08\x01\xFF",(new AddressPrefix('2','8',false,'FF00:0:0:0:0:0:0:0'))->serializeToWireFormat());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCalculateLength(): void{
+        self::assertSame(7,AddressPrefix::calculateLength("\x00\x01\x15\x03\xC0\xA8\x20"));
+        self::assertSame(7,AddressPrefix::calculateLength("\x00\x01\x15\x03\xC0\xA8\x20trailingBytes"));
+        self::assertSame(7,AddressPrefix::calculateLength("\x00\x01\x1C\x83\xC0\xA8\x26"));
+        self::assertSame(7,AddressPrefix::calculateLength("\x00\x01\x1C\x83\xC0\xA8\x26trailingBytes"));
+        self::assertSame(5,AddressPrefix::calculateLength("\x00\x02\x08\x01\xFF"));
+        self::assertSame(5,AddressPrefix::calculateLength("\x00\x02\x08\x01\xFFtrailingBytes"));
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromPresentationFormat(): void{
+        self::assertSame(['1','21',false,'192.168.32.0'],AddressPrefix::deserializeFromPresentationFormat('1:192.168.32.0/21')->getValue());
+        self::assertSame(['1','28',true,'192.168.38.0'],AddressPrefix::deserializeFromPresentationFormat('!1:192.168.38.0/28')->getValue());
+        self::assertSame(['2','8',false,'FF00:0:0:0:0:0:0:0'],AddressPrefix::deserializeFromPresentationFormat('2:FF00:0:0:0:0:0:0:0/8')->getValue());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromWireFormat(): void{
+        self::assertSame(['1','21',false,'192.168.32.0'],AddressPrefix::deserializeFromWireFormat("\x00\x01\x15\x03\xC0\xA8\x20")->getValue());
+        self::assertSame(['1','28',true,'192.168.38.0'],AddressPrefix::deserializeFromWireFormat("\x00\x01\x1C\x83\xC0\xA8\x26")->getValue());
+        self::assertSame(['2','8',false,'ff00::'],AddressPrefix::deserializeFromWireFormat("\x00\x02\x08\x01\xFF")->getValue());
+    }
+
+}
