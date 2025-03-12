65bb61a54a52c8f96de02d9225d45c4b642b8177
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-03-12 13:45:03 +0100

    Add option field

---
+++ src/Fields/Option.php
@@ -0,0 +1,84 @@
+<?php
+namespace YOCLIB\DNS\Fields;
+
+use YOCLIB\DNS\Exceptions\DNSFieldException;
+
+class Option implements Field{
+
+    private int $valueCode;
+    private string $valueData;
+
+    /**
+     * @param int $code
+     * @param string $data
+     * @throws DNSFieldException
+     */
+    public function __construct(int $code,string $data){
+        if($code<0 || $code>65535){
+            throw new DNSFieldException('Option code should be between 0 and 65535.');
+        }
+        $this->valueCode = $code;
+        $this->valueData = $data;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValue(): array{
+        return [$this->valueCode,$this->valueData];
+    }
+
+    /**
+     * @return string
+     * @throws DNSFieldException
+     */
+    public function serializeToPresentationFormat(): string{
+        throw new DNSFieldException('Cannot serialize option.');
+    }
+
+    /**
+     * @return string
+     */
+    public function serializeToWireFormat(): string{
+        return pack('n',$this->valueCode).pack('n',strlen($this->valueData)).$this->valueData;
+    }
+
+    /**
+     * @param string $data
+     * @return int
+     */
+    public static function calculateLength(string $data): int{
+        return 4 + unpack('n',str_pad(substr($data,2,2),2,"\x00",STR_PAD_LEFT))[1];
+    }
+
+    /**
+     * @param string $data
+     * @return Option
+     * @throws DNSFieldException
+     */
+    public static function deserializeFromPresentationFormat(string $data): Option{
+        throw new DNSFieldException('Cannot deserialize option.');
+    }
+
+    /**
+     * @param string $data
+     * @return Option
+     * @throws DNSFieldException
+     */
+    public static function deserializeFromWireFormat(string $data): Option{
+        if(strlen($data)<=4){
+            throw new DNSFieldException('Option should be at least 4 octets.');
+        }
+
+        $code = unpack('n',substr($data,0,2))[1];
+        $length = unpack('n',substr($data,2,2))[1];
+
+        $data = substr($data,4,$length);
+        if(strlen($data)!==$length){
+            throw new DNSFieldException('Too less data available to option.');
+        }
+
+        return new self($code,$data);
+    }
+
+}
---
+++ tests/Fields/OptionTest.php
@@ -0,0 +1,105 @@
+<?php
+namespace YOCLIB\DNS\Tests\Fields;
+
+use PHPUnit\Framework\TestCase;
+
+use YOCLIB\DNS\Exceptions\DNSFieldException;
+use YOCLIB\DNS\Fields\Option;
+
+class OptionTest extends TestCase{
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testConstructor(): void{
+        self::assertInstanceOf(Option::class,new Option(19,"\x02\x00\x78\x95\xA4\xE9"));
+    }
+
+    /**
+     * @return void
+     */
+    public function testConstructorCodeTooLarge(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Option code should be between 0 and 65535.');
+
+        new Option(1234567,"\x02\x00\x78\x95\xA4\xE9");
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testGetValue(): void{
+        self::assertSame([19,"\x02\x00\x78\x95\xA4\xE9"],(new Option(19,"\x02\x00\x78\x95\xA4\xE9"))->getValue());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testSerializeToPresentationFormat(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Cannot serialize option.');
+
+        (new Option(19,"\x02\x00\x78\x95\xA4\xE9"))->serializeToPresentationFormat();
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testSerializeToWireFormat(): void{
+        self::assertSame("\x00\x13\x00\x06\x02\x00\x78\x95\xA4\xE9",(new Option(19,"\x02\x00\x78\x95\xA4\xE9"))->serializeToWireFormat());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCalculateLength(): void{
+        self::assertSame(10,Option::calculateLength("\x00\x13\x00\x06\x02\x00\x78\x95\xA4\xE9"));
+        self::assertSame(10,Option::calculateLength("\x00\x13\x00\x06\x02\x00\x78\x95\xA4\xE9trailingBytes"));
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromPresentationFormat(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Cannot deserialize option.');
+
+        Option::deserializeFromPresentationFormat('abcdef');
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromWireFormat(): void{
+        self::assertSame([19,"\x02\x00\x78\x95\xA4\xE9"],Option::deserializeFromWireFormat("\x00\x13\x00\x06\x02\x00\x78\x95\xA4\xE9")->getValue());
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromWireFormatTooLessData(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Option should be at least 4 octets.');
+
+        Option::deserializeFromWireFormat("\xAA\xBB\xCC");
+    }
+
+    /**
+     * @return void
+     * @throws DNSFieldException
+     */
+    public function testDeserializeFromWireFormatTooLessOptionData(): void{
+        self::expectException(DNSFieldException::class);
+        self::expectExceptionMessage('Too less data available to option.');
+
+        Option::deserializeFromWireFormat("\x00\x13\x00\x04\xAA\xBB");
+    }
+
+}
